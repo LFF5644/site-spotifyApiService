9ca7cd7a9bb6a65092f9b3a6718e55e5a2cc69b8
LFF5644 (LFF-Server) <meerschweinchen5644@gmail.com>
2024-10-09 22:19:40 +0200

    unfinished but working state

--- spotify.service.js
+++ spotify.service.js
@@ -11,34 +11,76 @@ const services={
 	account: service_require("server/account/account.new"),
 };
 
+const tmpSAV=(data)=>{
+	fs.writeFileSync("/tmp/tmpSave.json",JSON.stringify(data,null,"\t"));
+}
+
 const configFile="data/spotifyLoginData.json";
 const URLS={
 	"authorize":	"https://accounts.spotify.com/authorize",
 	"devices":		"https://api.spotify.com/v1/me/player/devices",
+	"mytracks":		"https://api.spotify.com/v1/me/tracks", //my collection (lieblingssongs)
 	"next":			"https://api.spotify.com/v1/me/player/next",
 	"nowPlaying": 	"https://api.spotify.com/v1/me/player/currently-playing",
 	"pause": 		"https://api.spotify.com/v1/me/player/pause",
 	"play":			"https://api.spotify.com/v1/me/player/play",
 	"player":		"https://api.spotify.com/v1/me/player",
+	"playlist":		"https://api.spotify.com/v1/playlists",
 	"playlists": 	"https://api.spotify.com/v1/me/playlists",
+	"position":		"https://api.spotify.com/v1/me/player/seek",
 	"previous":		"https://api.spotify.com/v1/me/player/previous",
 	"shuffle":		"https://api.spotify.com/v1/me/player/shuffle",
 	"token":		"https://accounts.spotify.com/api/token",
 	"tracks":		"https://api.spotify.com/v1/playlists/[playListId]/tracks",
 	"volume":		"https://api.spotify.com/v1/me/player/volume",
 };
+const TEMPLATES={
+	track:{
+		id: null,
+		imgs: null,
+		length: null,
+		mp3: null,
+		name: null,
+		url: null,
+	},
+	playlist:{
+		description: null,
+		id: null,
+		imgs: null,
+		items: null,
+		name: null,
+		ownerId: null,
+		ownerName: null,
+		ownerUrl: null,
+		public: null,
+		total: null,
+		url: null,
+	},
+	device:{
+		active: null,
+		id: null,
+		name: null,
+		type: null,
+		volume: null,
+		volumeSupport: null,
+	},
+}
 
 this.start=()=>{
 	this.clients=new Map();
 	this.config=this.readConf();
 	this.infos_raw=null;
 	this.infos=null;
+	this.playlists=new Map();
+	this.tracks=new Map();
 	this.info_last={
 		deviceId: null,
+		lastSend: 0,
 		playing: null,
 		progress: null,
+		sourceId: null,
 		trackId: null,
-		lastSend: 0,
+		volume: null,
 	};
 
 	this.io=new socketIo.Server(13756,{cors:{origin:"*"}});
@@ -89,13 +131,39 @@ this.start=()=>{
 				cb(this.infos);
 			}
 		});
+		socket.on("get-track",trackId=>{
+			if(!this.tracks.has(trackId)){
+				socket.emit("error","get-track","trackId dont exists!");
+				return;
+			}
+			socket.emit("set-track",trackId,this.tracks.get(trackId));
+		});
+		socket.on("get-playlist",playlistId=>{
+			if(!this.playlists.has(playlistId)){
+				socket.emit("error","get-playlists","playlistId dont exists!");
+				return;
+			}
+			socket.emit("set-playlist",playlistId,this.playlists.get(playlistId));
+		});
 		socket.on("disconnect",()=>{
 			this.clients.delete(id);
 		});
 
-		if(client.allowChangePlayback) socket.on("playbackAction",action=>{
-			this.playbackAction(action);
-		});
+		if(client.allowChangePlayback){
+			socket.on("playbackAction",this.playbackAction);
+			socket.on("set-volume",volume=>{
+				this.callApi({
+					method: "put",
+					url: URLS.volume+"?device_id"+this.info_last.deviceId+"&volume_percent="+Number(volume),
+				});
+			});
+			socket.on("set-position",position=>{
+				this.callApi({
+					method: "put",
+					url: URLS.position+"?device_id"+this.info_last.deviceId+"&position_ms="+Number(position),
+				});
+			});
+		}
 	});
 
 	if(this.config.access_token){
@@ -107,7 +175,7 @@ this.start=()=>{
 	}else if(this.config.refresh_token&&this.config.refresh_token){
 		this.refresh_access_token();
 	}
-	setInterval(()=>{
+	setInterval(()=>{ // IRGENDWIE TROZDEM ALLE 500ms WENN CLIENT VERBUNDEN DUMM!!! ABER WENN NICHT VERBUNDEN GEHT!
 		const now=Date.now();
 		const call=()=>{
 			this.callApi({
@@ -119,15 +187,15 @@ this.start=()=>{
 		}
 
 		if(
-			now-this.info_last.lastSend>300&&
+			now-this.info_last.lastSend>3e3&&
 			this.clients.size>0
 		) call();
 		else if(
 			now-this.info_last.lastSend>10e3&&
-			this.clients.size<1
+			this.clients.size===0
 		) call();
 
-	},200);
+	},5e3);
 }
 this.writeClient=(id,object)=>{
 	const client={
@@ -153,6 +221,63 @@ this.readConf=()=>{
 
 	return fileData;
 }
+this.createTemplate=(target,data)=>{
+	if(target==="track"){
+		return {
+			...TEMPLATES.track,
+			id: data.id,
+			imgs: data.album.images,
+			length: data.duration_ms,
+			mp3: data.preview_url,
+			name: data.name,
+			url: data.external_urls.spotify,
+		}
+	}
+	else if(target==="playlist"){
+		const items=[];
+		console.log(data.tracks.items)
+		for(let index=0; index<data.tracks.items.length; index+=1){
+			const item=data.tracks.items[index];
+			const trackId=item.track.id;
+			if(!this.tracks.has(trackId)){
+				log("Add-Track: "+item.track.name);
+				this.tracks.set(trackId,this.createTemplate("track",item.track));
+			}
+			items.push({
+				addedAt: Number(new Date(item.added_at)),
+				addedBy: item.added_by.id,
+				id: trackId,
+			});
+		};
+		const result={
+			...TEMPLATES.playlist,
+			description: data.description,
+			id: data.id,
+			imgs: data.images,
+			items,
+			name: data.name,
+			ownerId: data.owner.id,
+			ownerName: data.owner.display_name,
+			ownerUrl: data.owner.external_urls.spotify,
+			public: data.public,
+			total: data.tracks.total,
+			url: data.external_urls.spotify,
+		};
+		tmpSAV(result);
+		return result;
+	}
+	else if(target==="device"){
+		return {
+			...TEMPLATES.device,
+			active: data.is_active,
+			id: data.id,
+			name: data.name,
+			type: data.type,
+			volume: data.volume_percent,
+			volumeSupport: data.supports_volume,
+		}
+	}
+}
 this.HandleServerResponse=data=>{
 	const {
 		args,
@@ -161,64 +286,96 @@ this.HandleServerResponse=data=>{
 	}=data;
 	if(serverResponse.error){
 		const status=serverResponse.error.status;
+		log("err");
 		if(status==401){
 			this.refresh_access_token();
 		}
 		else if(status==503){
 			this.callApi(args);
 		}
+		else{
+			console.log(args,serverResponse);
+		}
 	}
-	if(clientRequest=="get track"){
+
+	if(clientRequest==="logdata"){
+		console.log("LOG-DATA",serverResponse);
+	}
+	else if(clientRequest==="get track"){
 		if(serverResponse.error){
 			this.infos=null;
 			return false;
 		}
+		if(!serverResponse){
+			this.infos=null;
+			this.infos_raw=null;
+			log("no server response!");
+			return false;
+		}
 		let i={};
 		let infos_raw;
+		fs.writeFileSync("/tmp/spotifyLastTrackRaw.json",JSON.stringify(serverResponse,null,"\t"));
 		try{
 			i={
 				playing: serverResponse.is_playing,
-				device:{
-					active: serverResponse.device.is_active,
-					id: serverResponse.device.id,
-					name: serverResponse.device.name,
-					type: serverResponse.device.type,
-					volume: serverResponse.device.volume_percent,
+				repeat: serverResponse.repeat_state,
+				shuffle: serverResponse.shuffle_state?(serverResponse.smart_shuffle?"smart-shuffle":"normal-shuffle"):"off",
+				progress: serverResponse.progress_ms,
+				source:{
+					type: serverResponse.context.type,
+					url: serverResponse.context.external_urls.spotify,
+					id: serverResponse.context.uri.split(":")[2],
+					//api: serverResponse.context.href,
 				},
-				track:{
-					id: serverResponse.item.id,
-					imgs: serverResponse.item.album.images,
-					length: serverResponse.item.duration_ms,
-					mp3: serverResponse.item.preview_url,
-					name: serverResponse.item.name,
-					progress: serverResponse.progress_ms,
-					url: serverResponse.item.external_urls.spotify,
-				},
+				device: this.createTemplate("device",serverResponse.device),
+				track: this.createTemplate("track",serverResponse.item),
+				
 			};
 			infos_raw=serverResponse;
 		}catch(e){
 			i=null;
 			infos_raw=null;
-			//log("cant load data");
+			log("cant load data");
+			//console.log(e);
 		}
 		this.infos=i;
 		this.infos_raw=infos_raw;
 
 		// tell sockets changes
 		if(i.device.id!==this.info_last.deviceId) this.io.emit("change-device",i.device);
+		if(i.device.volume!==this.info_last.volume) this.io.emit("change-volume",i.device.volume);
 		if(i.playing!==this.info_last.playing) this.io.emit("change-playing",i.playing);
-		if(i.track.progress!==this.info_last.progress) this.io.volatile.emit("change-progress",i.track.progress);
+		if(i.progress!==this.info_last.progress) this.io.volatile.emit("change-progress",i.progress);
 		if(i.track.id!==this.info_last.trackId) this.io.emit("change-track",i.track);
+		if(i.source.id!==this.info_last.sourceId) this.io.emit("change-source",i.source);
+
+		if(i.source.type==="playlist"&&!this.playlists.has(i.source.id)){
+			this.callApi({
+				method: "get",
+				url: URLS.playlist+"/"+i.source.id,
+				request: "get playlist",
+			});
+		}
 
 		this.info_last={
 			...this.info_last,
 			deviceId: i.device.id,
 			playing: i.playing,
-			progress: i.track.progress,
+			progress: i.progress,
+			sourceId: i.source.id,
 			trackId: i.track.id,
+			volume: i.device.volume,
 		};
 	}
-	if(clientRequest=="get access_token"){
+	else if(clientRequest==="get playlist"){
+		const playlist=this.createTemplate("playlist",serverResponse);
+		if(!this.playlists.has(playlist.id)){
+			this.playlists.set(playlist.id,playlist);
+			log("Add-Playlist: "+playlist.name);
+		}
+		fs.writeFileSync("/tmp/spotifyLastPlaylist.json",JSON.stringify(serverResponse,null,"\t"));
+	}
+	else if(clientRequest==="get access_token"){
 		this.config.access_token=serverResponse.access_token;
 		this.saveConfig();
 	}
@@ -232,6 +389,7 @@ this.callApi=data=>{
 		request,
 		url,
 	}=data;
+	log("request: "+request);
 	fetch(url,{
 		method: method.toUpperCase(),
 		headers: {
@@ -293,7 +451,7 @@ this.playbackAction=(data)=>{
 	else if(action==="play"){
 		const {deviceId}=data;
 		const url=URLS.play+"?device_id="+(deviceId?deviceId:this.info_last.deviceId);
-		log(url);
+		//log(url);
 		this.callApi({
 			method: "put",
 			url,
